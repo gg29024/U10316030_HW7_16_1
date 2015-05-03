f0e7eb1027600b5f628485936b359706e6e72ec3
gg29024 <gg29024@gmail.com>
2015-05-04 02:07:01 +0800

    16.1_2

--- src/PickFourCards.java
+++ src/PickFourCards.java
@@ -1,32 +1,27 @@
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import java.awt.BorderLayout;
+
 import java.awt.*;
 import javax.swing.*;
-import java.awt.GridLayout;
-
+import java.awt.event.*;
 
 public class PickFourCards extends JFrame{
 	public static void main(String[] args) {
 	
-		PickFourCards frame = new PickFourCards();
-		
-		JButton jbtRefresh = new JButton("Refresh");
-		frame.add(jbtRefresh);
+		JFrame frame = new PickFourCards();
 		frame.setTitle("PickFourCards");
-		frame.setLocationRelativeTo(null); // Center the frame
+		frame.setLocationRelativeTo(null); 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(300, 350);
 		frame.setVisible(true);
 		
+	}
+	
+	public PickFourCards(){
+	
 		int[] deck = new int[52];
-		String[] suits = {"Spades", "Hearts", "Clubs", "Diamonds"};
-		String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
-        
 		// Initialize cards
 		for (int i = 0; i < deck.length; i++)
 		deck[i] = i;
-    
+		
 		// Shuffle the cards
 		for (int i = 0; i < deck.length; i++) {
 			// Generate an index randomly
@@ -35,67 +30,57 @@ public class PickFourCards extends JFrame{
 			deck[i] = deck[index]; 
 			deck[index] = temp;
 		}
-
-		// Display the first four cards
-		for (int i = 0; i < 4; i++) {
-			String suit = suits[deck[i] / 13];
-			String rank = ranks[deck[i] % 13];
-			System.out.println("Card number " + deck[i] + ": " + rank + " of " + suit);
-		}
+	
 	}
 }
+
+
 
 class TestPanels extends JFrame {
 	public TestPanels() {
 		// Create panel p1 for the buttons and set GridLayout
 		JPanel p1 = new JPanel();
-		p1.setLayout(new GridLayout(4, 3));
+		p1.setLayout(new GridLayout(1, 1));
 
-		
-
-		p1.add(new JButton("" + 0));
-		p1.add(new JButton("Start"));
-		p1.add(new JButton("Stop"));
+		p1.add(new JButton("Refresh"));
 
 		// Create panel p2 to hold a text field and p1
 		JPanel p2 = new JPanel(new BorderLayout());
-		p2.add(new JTextField("Time to be displayed here"),
-		BorderLayout.NORTH);
+		p2.add(new JTextField("Time to be displayed here"),BorderLayout.NORTH);
 		p2.add(p1, BorderLayout.CENTER);
 
 		// add contents into the frame
 		add(p2, BorderLayout.EAST);
-		add(new JButton("Food to be placed here"),
-		BorderLayout.CENTER);
+		add(new JButton("Food to be placed here"),BorderLayout.CENTER);
 	}
 	
 }
 
 
-public class ShowGridLayout extends JFrame {
-  public ShowGridLayout() {
-    // Set GridLayout, 3 rows, 2 columns, and gaps 5 between
-    // components horizontally and vertically
-    setLayout(new GridLayout(3, 2, 5, 5));
+class ShowGridLayout extends JFrame {
+	public ShowGridLayout() {
+		// Set GridLayout, 3 rows, 2 columns, and gaps 5 between
+		// components horizontally and vertically
+		setLayout(new GridLayout(3, 2, 5, 5));
 
-    // Add labels and text fields to the frame
-    add(new JLabel("First Name"));
-    add(new JTextField(8));
-    add(new JLabel("MI"));
-    add(new JTextField(1));
-    add(new JLabel("Last Name"));
-    add(new JTextField(8));
-  }
+		// Add labels and text fields to the frame
+		add(new JLabel("First Name"));
+		add(new JTextField(8));
+		add(new JLabel("MI"));
+		add(new JTextField(1));
+		add(new JLabel("Last Name"));
+		add(new JTextField(8));
+	}
 
-  /** Main method */
-  public static void main(String[] args) {
-    ShowGridLayout frame = new ShowGridLayout();
-    frame.setTitle("ShowGridLayout");
-    frame.setSize(200, 125);
-    frame.setLocationRelativeTo(null); // Center the frame
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setVisible(true);
-  }
+	/** Main method */
+	public static void main(String[] args) {
+		ShowGridLayout frame = new ShowGridLayout();
+		frame.setTitle("ShowGridLayout");
+		frame.setSize(200, 125);
+		frame.setLocationRelativeTo(null); // Center the frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
 }
 
 
@@ -109,7 +94,6 @@ class ShowBorderLayout extends JFrame {
 		add(new JButton("South"), BorderLayout.SOUTH);
 		add(new JButton("West"), BorderLayout.WEST);
 		add(new JButton("North"), BorderLayout.NORTH);
-		add(new JButton("Center"), BorderLayout.CENTER);
 	}
 
 	/** Main method */
@@ -123,5 +107,30 @@ class ShowBorderLayout extends JFrame {
 	}
 }
 
+
+class TestImageIcon extends JFrame {
+	private ImageIcon usIcon = new ImageIcon("image/us.gif");
+	private ImageIcon myIcon = new ImageIcon("image/my.jpg");
+	private ImageIcon frIcon = new ImageIcon("image/fr.gif");
+	private ImageIcon ukIcon = new ImageIcon("image/uk.gif");
+
+	public TestImageIcon() {
+		setLayout(new GridLayout(1, 4, 5, 5));
+		add(new JLabel(usIcon));
+		add(new JLabel(myIcon));
+		add(new JButton(frIcon));
+		add(new JButton(ukIcon));
+	}
+
+	/** Main method */
+	public static void main(String[] args) {
+		TestImageIcon frame = new TestImageIcon();
+		frame.setTitle("TestImageIcon");
+		frame.setSize(500, 125);
+		frame.setLocationRelativeTo(null); // Center the frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
 
 
